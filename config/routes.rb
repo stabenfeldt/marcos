d49a287b0bcb7a5823a5ec46312400dbeb11731c
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-11-13 09:15:50 +0100

    Scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :services
   resources :bikes
+  resources :users
+  resources :customers
+  resources :customers
+  resources :customers
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
