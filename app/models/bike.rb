3ae58839ce142973f647000833474f1c13521c12
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-11-20 12:53:10 +0100

    div relationships

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,2 +1,11 @@
 class Bike < ActiveRecord::Base
+
+  validates :brand, :model, presence: true
+  belongs_to :customer
+  has_many :parts
+
+  def name
+    "#{brand} - #{model}"
+  end
+
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,7 @@
 class Service < ActiveRecord::Base
   belongs_to :bike
   belongs_to :user
+
+  validates :description, :due_date, :bike, :user,
+    presence: true
 end
