9f0e6cafef85bbe32a0ddbf4addae518f20d78f5
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-11-20 12:54:04 +0100

    added breadcrumbs, body_id and flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def body_id
+    "#{controller.controller_name}_#{controller.action_name}"
+  end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-error", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+              concat message
+      end)
+    end
+    nil
+  end
 end
