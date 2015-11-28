5f24717024f290b941a76632be3507d3f9636017
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-11-28 13:00:00 +0100

    scope

--- app/models/service.rb
+++ app/models/service.rb
@@ -9,10 +9,14 @@
 #  bike_id     :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  completed   :boolean          default(FALSE)
 #
 
 class Service < ActiveRecord::Base
   belongs_to :bike
+
+  scope :in_progress, -> { where(completed: false) }
+  scope :completed,   -> { where(completed: true)  }
 
   def completed?
     completed
