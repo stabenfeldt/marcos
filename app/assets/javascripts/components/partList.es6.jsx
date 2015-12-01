42d41e13ccf57c9566b44dd58543921d72051046
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-12-01 11:25:55 +0100

    listing parts react style

--- app/assets/javascripts/components/partList.es6.jsx
+++ app/assets/javascripts/components/partList.es6.jsx
@@ -1,7 +1,12 @@
 class PartList extends React.Component {
   render () {
-    return (
-
+      var list = this.props.items.map(function (element) {
+          return <Part {...element} />
+      });
+    return(
+        <div>
+        {list}
+        </div>
     );
   }
 }
