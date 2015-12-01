722e6bc1b67525f3885c38248b0514d89033f018
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-12-01 13:30:56 +0100

    Select part for the bike on /customers/6/bikes/10/parts

--- app/assets/javascripts/components/part.es6.jsx
+++ app/assets/javascripts/components/part.es6.jsx
@@ -1,11 +1,13 @@
 class Part extends React.Component {
   render () {
     return (
-        <div className="list-group-item">
-            <div className="pull-right">
+        <div className="list-group-item" key={this.props.id}>
+
+            <div className={this.props.hide_menu ? 'hidden' : 'pull-right'}>
               <a href={'parts/'+this.props.id+'/edit'}> Rediger </a>
               <a href={'parts/'+this.props.id} data-method="delete"> Slett </a>
             </div>
+
             <h4> {this.props.brand} - {this.props.model} </h4>
             <em> {this.props.year} </em>
             <span>{this.props.note}</span>
@@ -18,5 +20,5 @@ Part.propTypes = {
   brand: React.PropTypes.string,
   model: React.PropTypes.string,
   year: React.PropTypes.node,
-  note: React.PropTypes.string
+  note: React.PropTypes.string,
 };
--- app/assets/javascripts/components/partList.es6.jsx
+++ app/assets/javascripts/components/partList.es6.jsx
@@ -1,7 +1,8 @@
 class PartList extends React.Component {
   render () {
       var list = this.props.items.map(function (element) {
-          return <Part {...element} />
+          console.log("elemetnt: ", element);
+          return <Part {...element} hide_menu="true" />
       });
     return(
         <div className="list-group">
