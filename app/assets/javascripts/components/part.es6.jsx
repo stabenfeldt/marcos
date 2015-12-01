8eeebf5a376e38d8b9e2f16438a8dd4e265f073a
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-12-01 14:43:39 +0100

    click alert works.

--- app/assets/javascripts/components/part.es6.jsx
+++ app/assets/javascripts/components/part.es6.jsx
@@ -1,15 +1,20 @@
 class Part extends React.Component {
+    handleClick () {
+        alert('hi')
+    }
     render () {
         return (
-            <div className="list-group-item" key={this.props.id} onClick={this.props.whenClicked} >
-              <div className={this.props.hide_menu ? 'hidden' : 'pull-right'}>
-                <a href={'parts/'+this.props.id+'/edit'}> Rediger </a>
-                <a href={'parts/'+this.props.id} data-method="delete"> Slett </a>
-              </div>
+            <div className="list-group-item" key={this.props.id}
+            onClick={this.handleClick}>
 
-              <h4> {this.props.brand} - {this.props.model} </h4>
-              <em> {this.props.year} </em>
-              <span>{this.props.note}</span>
+            <div className={this.props.hide_menu ? 'hidden' : 'pull-right'}>
+            <a href={'parts/'+this.props.id+'/edit'}> Rediger </a>
+            <a href={'parts/'+this.props.id} data-method="delete"> Slett </a>
+            </div>
+
+            <h4> {this.props.brand} - {this.props.model} </h4>
+            <em> {this.props.year} </em>
+            <span>{this.props.note}</span>
             </div>
         );
     }
