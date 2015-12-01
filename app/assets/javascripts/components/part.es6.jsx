fc3eec042701f642f17c6e81936783beea9f5a0f
Martin Stabenfeldt <martin@stabenfeldt.net>
2015-12-01 14:40:47 +0100

    indenting

--- app/assets/javascripts/components/part.es6.jsx
+++ app/assets/javascripts/components/part.es6.jsx
@@ -1,24 +1,23 @@
 class Part extends React.Component {
-  render () {
-    return (
-        <div className="list-group-item" key={this.props.id}>
+    render () {
+        return (
+            <div className="list-group-item" key={this.props.id} onClick={this.props.whenClicked} >
+              <div className={this.props.hide_menu ? 'hidden' : 'pull-right'}>
+                <a href={'parts/'+this.props.id+'/edit'}> Rediger </a>
+                <a href={'parts/'+this.props.id} data-method="delete"> Slett </a>
+              </div>
 
-            <div className={this.props.hide_menu ? 'hidden' : 'pull-right'}>
-              <a href={'parts/'+this.props.id+'/edit'}> Rediger </a>
-              <a href={'parts/'+this.props.id} data-method="delete"> Slett </a>
+              <h4> {this.props.brand} - {this.props.model} </h4>
+              <em> {this.props.year} </em>
+              <span>{this.props.note}</span>
             </div>
-
-            <h4> {this.props.brand} - {this.props.model} </h4>
-            <em> {this.props.year} </em>
-            <span>{this.props.note}</span>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 Part.propTypes = {
-  brand: React.PropTypes.string,
-  model: React.PropTypes.string,
-  year: React.PropTypes.node,
-  note: React.PropTypes.string,
+    brand: React.PropTypes.string,
+    model: React.PropTypes.string,
+    year: React.PropTypes.node,
+    note: React.PropTypes.string
 };
