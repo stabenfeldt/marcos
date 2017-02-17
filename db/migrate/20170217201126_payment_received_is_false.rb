3551f6e3a17b2b71bbc8ca49f2cae1109ebc4900
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-17 23:01:46 +0100

    update migrations

--- db/migrate/20170217201126_payment_received_is_false.rb
+++
@@ -1,5 +0,0 @@
-class PaymentReceivedIsFalse < ActiveRecord::Migration
-  def change
-    change_column :orders, :payment_received, :boolean, :default => false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217202443) do
+ActiveRecord::Schema.define(version: 20170217212026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 20170217202443) do
     t.string   "phone"
     t.string   "email"
     t.boolean  "payment_received", default: false
+    t.string   "bike_brand"
+    t.string   "bike_model"
+    t.string   "description"
     t.date     "valid_through"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
-    t.string   "bike_brand"
-    t.string   "bike_model"
-    t.text     "description"
   end
 
   add_index "orders", ["product_id"], name: "index_orders_on_product_id", using: :btree
