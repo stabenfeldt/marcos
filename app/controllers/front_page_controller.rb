047676c656d076f13ebd3e1e3864f22520d2eddb
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-17 23:02:41 +0100

    fix flash notice

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -8,14 +8,14 @@ class FrontPageController < ApplicationController
   def contact_form
     @order = Order.create!(
       name: params[:name],
-      product: params[:product],
+      product_id: params[:product_id],
       phone: params[:phone],
       email: params[:email],
       bike_brand: params[:bike_brand],
       bike_model: params[:bike_model],
     )
     Rails.logger.debug "Order created: #{@order.inspect}"
-    flash[:info] = 'Takk for din bestilling!'
+    flash[:notice] = 'Takk for din bestilling!'
     redirect_to fp_url
   end
 
