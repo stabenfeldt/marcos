3bf433eaae1b63c4238ed3698f10ad76fc76f5ae
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-18 14:30:32 +0100

    update selected product when clicking button

--- app/assets/javascripts/front_page.js
+++ app/assets/javascripts/front_page.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function selectOrder(order, that) {
+  console.log(order + " was selected");
+  $('input.' + order).attr('checked', 'checked');
+}
