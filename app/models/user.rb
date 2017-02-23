2185d913520c4950cc8655e18199174ed0c144f4
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-23 10:24:54 +0100

    s/mobile/phone/

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@
 #  first_name :string
 #  last_name  :string
 #  email      :string
-#  mobile     :string
+#  phone     :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
