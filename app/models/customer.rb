e907b2afd4c0b4432c95f21a79b81318d70f140a
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-23 10:24:02 +0100

    s/mobile/phone/

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@
 #  id         :integer          not null, primary key
 #  first_name :string
 #  last_name  :string
-#  mobile     :string
+#  phone      :string
 #  email      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -15,8 +15,8 @@ class Customer < ActiveRecord::Base
 
   has_many :bikes
   has_many :services, through: :bikes
-  validates :first_name, :last_name, :mobile, presence: true
-  validates :mobile, :email, uniqueness: true, allow_blank: true
+  validates :first_name, :last_name, :phone, presence: true
+  validates :phone, :email, uniqueness: true, allow_blank: true
 
   scope :services_in_progress, -> { where(field: value) }
 
