baf7e89ade28e4931687695f1dfbb830f05c94e6
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-28 13:35:11 +0100

    Omniauth search helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,27 @@
 
 # The User class is used for authenication for the system.
 class User < ActiveRecord::Base
+
+  def self.from_omniauth(auth_params)
+    Rails.logger.debug "auth_params: #{auth_params.to_json}"
+    where(auth_params.slice("provider", "uid")).first || create_from_omniauth(auth_params)
+  end
+
+  def self.create_from_omniauth(auth_params)
+    uid = auth_params.uid
+    provider = auth_params.provider
+    name = auth_params.info.name
+
+    create! do |user|
+      user.provider   = provider
+      user.uid        = uid
+      user.first_name, user.last_name = name
+    end
+  end
+
+  private
+
+
+
+
 end
