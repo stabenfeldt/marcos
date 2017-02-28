52e743ee34ebbec1ba5f66ef946ead77ba4e839a
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-02-28 13:35:35 +0100

    OmniAuth

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,12 @@ module Marcos
         request_specs: false
     end
 
+
+    # OmniAuth
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
   end
 end
 
