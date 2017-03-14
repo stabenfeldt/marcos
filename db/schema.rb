3348653b5898a1b73b2b92f4d3d8c8d0dd9a4a9e
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-03-14 14:22:28 +0100

    Add username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228100303) do
+ActiveRecord::Schema.define(version: 20170314130306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20170228100303) do
     t.datetime "updated_at", null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "username"
   end
 
   add_foreign_key "bikes", "customers"
