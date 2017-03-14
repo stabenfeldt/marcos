08a0dcd8ecbb57d00d702865c6c90ab90cad6115
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-03-14 15:12:35 +0100

    add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314130306) do
+ActiveRecord::Schema.define(version: 20170314133158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,11 +96,12 @@ ActiveRecord::Schema.define(version: 20170314130306) do
     t.string   "last_name"
     t.string   "email"
     t.string   "mobile"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "username"
+    t.string   "role",       default: "normal"
   end
 
   add_foreign_key "bikes", "customers"
