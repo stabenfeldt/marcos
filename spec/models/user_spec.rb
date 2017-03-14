4d69178a596a093c79c88798f62643ff2ef114bd
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-03-14 15:13:19 +0100

    test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,5 +17,9 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'admin?' do
+    user = Fabricate(:user, role: :admin)
+    expect(user.admin?).to be true
+  end
 end
