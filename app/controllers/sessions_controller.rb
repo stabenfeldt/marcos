944a3d1b99cafc9f67c94d4dd7072028a0eece93
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-03-17 11:42:10 +0100

    Moved logic to the controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,7 @@ class SessionsController < ApplicationController
   def create
     # First receive the user code from Strava
     code = auth_params["code"]
-    user_data = fetch_user_data(code)
-    user = User.from_strava_omniauth(user_data)
+    user = User.from_strava_omniauth(code)
     session[:user_id] = user.id
     redirect_to root_url, notice: "Welcome"
   end
@@ -16,14 +15,6 @@ class SessionsController < ApplicationController
 
   protected
 
-	def fetch_user_data(code)
-    client_id     = ENV["STRAVA_CLIENT_ID"]
-    client_secret = ENV["STRAVA_CLIENT_SECRET"]
-    HTTParty.post( "https://www.strava.com/oauth/token?" +
-                   "client_id=#{client_id}" +
-                   "&client_secret=#{client_secret}" +
-                   "&code=#{code}")
-	end
 
   def auth_hash
     Rails.logger.debug "AUTH HASH: "
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,22 @@ class User < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
-  def self.from_strava_omniauth(user_data)
+  def fetch_bikes_from_strava
+    bikes = user_data["athlete"]["bikes"].map { |bike| OpenStruct.new(bike) }
+    Rails.logger.debug "GOT BIKES: #{bikes.first}"
+  end
+
+  def self.fetch_user_data(code)
+    client_id     = ENV["STRAVA_CLIENT_ID"]
+    client_secret = ENV["STRAVA_CLIENT_SECRET"]
+    HTTParty.post( "https://www.strava.com/oauth/token?" +
+                   "client_id=#{client_id}" +
+                   "&client_secret=#{client_secret}" +
+                   "&code=#{code}")
+	end
+
+  def self.from_strava_omniauth(code)
+    user_data = fetch_user_data(code)
     username = user_data.parsed_response["athlete"]["username"]
     where( { username: username, provider: :strava }).first || create_from_omniauth(user_data)
   end
