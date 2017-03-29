6addb540dba9eb26e78754afe6b575be8fc46022
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-03-29 13:12:51 +0200

    basic tests

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -15,5 +15,17 @@
 require 'rails_helper'
 
 RSpec.describe Service, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    Part.create!(brand: 'generic', model: 'generic', kind: 'rear shock',
+                 service_interval: 3000,
+                 note: 'Rear shocks need new oil every 3000 km')
+    @cassette = Part.create!(brand: 'generic', model: 'generic', kind: 'cassette',
+                 service_interval: 3000, note: '')
+    @bike = Fabricate(:bike)
+    @service = @cassette.services.new
+  end
+
+  it 'is valid from the fabric' do
+    expect(@service).to be_valid
+  end
 end
