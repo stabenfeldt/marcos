444cfe112277fd04df1b494a6b1ea352bdc917d0
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-04-18 10:54:56 +0200

    update redirect

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -79,7 +79,7 @@ class ServicesController < ApplicationController
   def destroy
     @service.destroy
     respond_to do |format|
-      format.html { redirect_to services_url, notice: 'Service was successfully destroyed.' }
+      format.html { redirect_to users_url(@service.bike.user), notice: 'Service was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
