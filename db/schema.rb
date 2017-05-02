a3212b4cad6bae6bedcb64e1e2352a96d94239ba
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-05-02 13:36:56 +0200

    put back missing services migration

--- db/schema.rb
+++ db/schema.rb
@@ -106,13 +106,12 @@ ActiveRecord::Schema.define(version: 20170428115338) do
   end
 
   create_table "services", force: :cascade do |t|
-    t.integer  "bike_id"
+    t.string   "description"
+    t.string   "log"
     t.date     "due_date"
-    t.text     "description"
-    t.text     "log"
+    t.integer  "bike_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.boolean  "completed"
   end
 
   add_index "services", ["bike_id"], name: "index_services_on_bike_id", using: :btree
