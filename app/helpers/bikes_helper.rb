3a2ee5da381e81e762b588063e43023d295ca3fe
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-06-21 09:05:01 +0200

    Better styling for each state of the parts

--- app/helpers/bikes_helper.rb
+++ app/helpers/bikes_helper.rb
@@ -7,16 +7,16 @@ module BikesHelper
     km_until_next = bike_part.km_until_next_service
 
     if bike_part.in_for_service?
-      return "Til service nå"
+      return ["Til service nå", :service_now]
     end
 
     if km_until_next == 0
-      return "Nå"
+      return ["Nå", :now]
     elsif (km_until_next < 0)
-      return " #{number_to_human(km_until_next/100, unit: :distance)} kilometer siden"
-      #return km_until_next.to_i
+      return [" #{number_to_human( -1* km_until_next/100, unit: :distance, precision: 0)} kilometer siden", :over]
+      #return [" #{km_until_next/100} kilometer siden", :over]
     elsif (km_until_next > 0)
-      return "Om #{number_to_human(km_until_next, unit: :distance)} kilometer"
+      return ["Om #{number_to_human(km_until_next/100, unit: :distance)} kilometer", :soon]
     end
 
   end
