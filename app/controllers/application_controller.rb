2a4232b40dc92a5505ec5ebdf2effc2e904d1357
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-08-11 21:45:18 +0200

    allow extra params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   protect_from_forgery with: :null_session,
     only: Proc.new { |c| c.request.format.json? },
@@ -22,5 +24,9 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user, :signed_in?, :admin?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 
 end
