21f023b0cf26fc7dddcc14a22004d2099fceab27
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-09-06 21:01:18 +0200

    started on the notification_mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,2 +1,10 @@
 class NotificationMailer < ApplicationMailer
+
+  def bike_due(bike:, owner:)
+    @bike = bike
+    @owner = owner
+    @url  = 'http://example.com/login'
+    mail(to: @owner.email, subject: "Your bike #{@bike.name} needs service")
+  end
+
 end
