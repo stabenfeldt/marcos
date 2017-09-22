5ed59daf38a928a9c19f537fb140d38fd5205fd9
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-09-22 20:08:50 +0200

    fix indent

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  config.before(:suite) do
+   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
