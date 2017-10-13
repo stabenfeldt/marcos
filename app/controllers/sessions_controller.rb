6bd20bc84e234b2bd8d8921861e91f506d222499
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-10-13 13:57:51 +0200

    fix bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController
+class SessionsController < Devise::SessionsController
   def create
     # First receive the user code from Strava
     code = auth_params["code"]
