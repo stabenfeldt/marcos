4f0dafb464ac5f9128af9c7cff5693f0716d9de0
Martin Stabenfeldt <martin@stabenfeldt.net>
2017-11-26 21:52:09 +0100

    A user can update his own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update ]
-  before_action :only_admins, except: [:show, :fetch_bikes_from_strava]
+  before_action :only_admins, except: [:show, :fetch_bikes_from_strava, :edit, :update]
   #before_action :authenticate_user!
 
 
